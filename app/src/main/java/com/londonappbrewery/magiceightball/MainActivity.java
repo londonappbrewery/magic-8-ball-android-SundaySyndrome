35849614a788f5d6a2bf4c885fd1e251490587e1
SundaySyndrome <victorr.almeida@yahoo.com>
2019-10-05 22:04:36 -0300

    Project complete

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,12 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button askButton = findViewById(R.id.askButton);
+        final ImageView ballImage = findViewById(R.id.image_8Ball);
+
+        final int[] predictionImageArray = {
+            R.drawable.ball1,
+            R.drawable.ball2,
+            R.drawable.ball3,
+            R.drawable.ball4,
+            R.drawable.ball5,
+        };
+
+        askButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random randomPredictionGenerator = new Random();
+                int randomPrediction = randomPredictionGenerator.nextInt(predictionImageArray.length);
+
+                ballImage.setImageResource(predictionImageArray[randomPrediction]);
+            }
+        });
     }
 }
